d48c14baf4ad705432906e24410131202e0483cc
ivana092 <ivanapathak8898@gmail.com>
2019-01-16 20:14:28 +0530

    sending only build files

---
+++ build/precache-manifest.b55b09e17d0a2360a231b13a8e2687a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8b0c4eef367d52e537ac",
+    "url": "/static/js/main.8b0c4eef.chunk.js"
+  },
+  {
+    "revision": "66fe43aec27199ad423d",
+    "url": "/static/js/1.66fe43ae.chunk.js"
+  },
+  {
+    "revision": "8b0c4eef367d52e537ac",
+    "url": "/static/css/main.19c29601.chunk.css"
+  },
+  {
+    "revision": "d7caa00c981afef3621994ed05e84dd3",
+    "url": "/index.html"
+  }
+];
